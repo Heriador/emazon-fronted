75d5d118b25b0e9452b7968650ec7208e8ca8838
angel andres miranda castillo <mirandaangel83@gmail.com>
2024-10-22 14:22:31 -0500

    feat: created card component to display the items info

---
+++ src/app/shared/components/organisims/card/card.component.ts
@@ -0,0 +1,53 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { TextType } from 'src/app/shared/constants/enums';
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+
+  TextType = TextType;
+  faPlus = faPlus;
+  faMinus = faMinus;
+
+  @Input() title: string = '';
+  @Input() description: string = '';
+  @Input() brand: string = '';
+  @Input() price: string = '';
+  @Input() stock: number = 0;
+  @Input() categories: string[] = [];
+  @Output() selectedQuantity: EventEmitter<number> = new EventEmitter<number>();
+
+  quantity: number = 1;
+  showMore: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getMarca(): string{
+    return `<strong>Marca:</strong> ${this.brand}`
+  }
+
+  togleShowMore(){
+    this.showMore = !this.showMore;
+  }
+
+  increaseQuantity(){
+    if(this.quantity === this.stock) return;
+    this.quantity++;
+    this.selectedQuantity.emit(this.quantity);
+  }
+
+  decreaseQuantity(){
+    if(this.quantity > 1){
+      this.quantity--;
+      this.selectedQuantity.emit(this.quantity);
+    }
+  }
+
+
+}
