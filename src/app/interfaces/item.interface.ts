0a00628b0987d3566d57d290a39e858ac99d7422
angel andres miranda castillo <mirandaangel83@gmail.com>
2024-10-22 14:22:06 -0500

    feat: created service function to get the items paginated

--- src/app/interfaces/item.interface.ts
+++ src/app/interfaces/item.interface.ts
@@ -1,3 +1,5 @@
+import { BrandResponse } from "./brand.interface";
+import { CategoryResponse } from "./category.interface";
 
 export interface Item {
     name: string;
@@ -6,4 +8,14 @@ export interface Item {
     stock: number;
     brandId: number;
     categories: number[];
+}
+
+export interface ItemResponse {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    stock: number;
+    brand: BrandResponse;
+    categories: CategoryResponse[];
 }
